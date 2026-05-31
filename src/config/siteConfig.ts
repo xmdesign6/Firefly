af2e144effa14f6311f077aad10fb76cc100e0f1
xmdesign6 <74769363+xmdesign6@users.noreply.github.com>
2026-06-01 00:13:19 +0800

    更新 siteConfig.ts

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -7,22 +7,22 @@ const SITE_LANG = "zh_CN";
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "极客指北",
+	title: "时光碎语",
 
 	// 站点副标题
-	subtitle: "Demo site",
+	subtitle: "关于生活、设计与美学的碎片收集",
 
 	// 站点 URL
 	site_url: "https://blog.90svip.cn",
 
 	// 站点描述
 	description:
-		"Firefly 是一款基于 Astro 框架和 Fuwari 模板开发的清新美观且现代化个人博客主题模板，专为技术爱好者和内容创作者设计。该主题融合了现代 Web 技术栈，提供了丰富的功能模块和高度可定制的界面，让您能够轻松打造出专业且美观的个人博客网站。",
+		"捕捉日常的棱角与光影。这里存放着我对世界的观察，以及那些稍纵即逝的灵感切片。",
 
 	// 站点关键词
 	keywords: [
-		"Firefly",
-		"Fuwari",
+		"音乐",
+		"摄影",
 		"Astro",
 		"ACGN",
 		"博客",
@@ -91,7 +91,7 @@ export const siteConfig: SiteConfig = {
 	},
 
 	// 站点开始日期，用于统计运行天数
-	siteStartDate: "2025-01-01",
+	siteStartDate: "2026-06-01",
 
 	// 站点时区（IANA 时区字符串），用于格式化bangumi、rss里的构建日期时间等等..
 	// 示例："Asia/Shanghai", "UTC", 如果为空，则按照构建服务器的时区进行时区转换
