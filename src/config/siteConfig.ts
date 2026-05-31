99c66ee0ed22ba3ede9e4d4d57013f6564601a43
xmdesign6 <74769363+xmdesign6@users.noreply.github.com>
2026-05-31 11:57:46 +0800

    Update firefly.md

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -7,13 +7,13 @@ const SITE_LANG = "zh_CN";
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "Firefly",
+	title: "极客指北",
 
 	// 站点副标题
 	subtitle: "Demo site",
 
 	// 站点 URL
-	site_url: "https://firefly.cuteleaf.cn",
+	site_url: "https://blog.90svip.cn",
 
 	// 站点描述
 	description:
